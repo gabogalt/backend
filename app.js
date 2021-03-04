69679e7e7e6a83c89730be0ac6a07d3935fe5080
gabogalt <gabogalt34@gmail.com>
2021-03-04 11:29:07 -0300

    Definimos ruta user

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ var express = require("express");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
+// Requerimos las rutas que vamos a usar
+const user = require("./routes/user");
+
 var app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Usamos las rutas
+app.use("/user", user);
 
 // catch 404 and forward to error handler
 app.use((_, res) => {
